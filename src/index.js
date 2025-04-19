0c7ffc1d8c05d3b47b8584d40f7dd0eafae5d8e5
Ruben feng <rfeng@ayudinga.org>
2025-04-19 16:26:13 -0500

    fix: simplify database connection logic by removing unnecessary connectDB function

--- src/index.js
+++ src/index.js
@@ -4,18 +4,13 @@ const app = express();
 const Usuario = require('./models/Usuario');
 require('dotenv').config();
 
-let isConnected = false;
 
-const connectDB = async () => {
-  if (isConnected) return;
-  await mongoose.connect(process.env.MONGODB_URI);
-  isConnected = true;
-};
+
+mongoose.connect(process.env.MONGODB_URI);
 
 app.use(express.json());
 app.get('/users', async(req, res) => {
   try{
-      await connectDB();
       const users = await Usuario.find({});
       res.status(200).json(users);
     }catch(err){
