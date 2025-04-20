91c4387cf714a078fca26b2482e6b188c96e6f1d
Ruben feng <rfeng@ayudinga.org>
2025-04-20 12:13:53 -0500

    fix: update user retrieval and creation logic, enhance error handling, and modify task management in Usuario model

--- src/index.js
+++ src/index.js
@@ -10,15 +10,65 @@ app.use(cors());
 mongoose.connect(process.env.MONGO_URL);
 
 app.use(express.json());
-app.get('/users', async(req, res) => {
+app.get('/users/:ip', async(req, res) => {
   try{
-      const users = await Usuario.find();
-      res.status(200).json(users);
+      const user = await Usuario.findOne({ ip: req.params.ip });
+      if (!user) return res.status(404).send('User not found');
+      res.status(200).json(user);
     }catch(err){
       res.status(500).json({message: err.message});
     }
   }
 );
+app.post('/users/:ip', async(req, res) => {
+
+  try {
+    if(Usuario.findOne({ ip: req.params.ip })){
+      return res.status(400).send('User already exists');
+    }
+    const newUser = new Usuario(req.body);
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+})
+
+app.post('/users/:ip/tasks', async(req, res) => {
+  try {
+    const user = await Usuario.findOne({ ip: req.params.ip });
+    if (!user) return res.status(404).send('User not found');
+
+    const newTask = {
+      homework: req.body.homework || '',
+      complete: req.body.complete || false
+    };
+    user.tasks.push(newTask);
+    await user.save();
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+})
+/**
+ * body=>
+ * id: task id
+ * homework: task name
+ * complete: task status
+ */
+app.put("/Users/:ip/tasks", async (req, res) => {
+  const user = await Usuario.findOne({ ip: req.params.ip });
+  if (!user) return res.status(404).send('User not found');
+  
+  user.tasks.map((taks)=> {
+    if(taks.id === req.body.id){
+      taks.homework = req.body.homework;
+      taks.complete = req.body.complete;  
+    }
+  });
+  await user.save();
+  res.status(200).json(user); 
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const tareaSchema = new mongoose.Schema({
   homework: {
     type: String,
-    required: true
+    default: '' 
   },
   complete: {
     type: Boolean,
